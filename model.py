1661b5aa5cf8298a87a2113c36cddf01db495271
root <root@DESKTOP-U3C1643.>
2024-06-25 16:13:57 -0700

    python-project-4

--- model.py
+++ model.py
@@ -12,12 +12,24 @@ class User(db.Model):
     username = db.Column(db.String(255), unique = True, nullable = False)
     password = db.Column(db.String(255), nullable = False)
 
+    teams = db.relationship("Team", backref = "user", lazy = True)
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
 class Team(db.Model):
     __tablename__ = "teams"
 
     id = db.Column(db.Integer, primary_key = True, autoincrement = True)
     team_name = db.Column(db.String(255), unique = True, nullable = False)
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable = False)
+    team = my_user.teams[0]
+
+    def __init__(self, team_name, user_id):
+        self.team_name = team_name
+        self.user_id = user_id
+
 
 
 
@@ -30,6 +42,12 @@ class Project(db.Model):
     description = db.Column(db.String(255), nullable = True)
     completed = db.Column(db.Boolean, default = False)
     team_id = db.Column(db.Integer, db.ForeignKey("teams.id"), nullable = False)
+
+    def __init__(self, project_name, completed, team_id, description=""):
+        self.project_name = project_name
+        self.completed = completed
+        self.team_id = team_id
+        self.description = description
 
 
 
